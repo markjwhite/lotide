eab2e61ece38e8025004330ab075ee97ed14c377
markjwhite <markwhite259@gmail.com>
2021-04-27 16:13:53 -0400

    checks if arrays are equal and logs assertion

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
