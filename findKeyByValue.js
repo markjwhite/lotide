7ded890c65141b36aaf08c552f7accbffd335d54
markjwhite <markwhite259@gmail.com>
2021-04-28 15:22:32 -0400

    iterates through and object to find key that matches input

---
+++ findKeyByValue.js
@@ -0,0 +1,16 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  for (let key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
+}
